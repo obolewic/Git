368d728fa51c275f660d41aa95c370d86aafd42f
obol <obolewicz.m@gmail.com>
2014-01-13 14:07:41 +0100

    Create Project
    Project in now created

---
+++ VisualProject/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout<<"Hi git majonez";
+	return 0;
+}
+
